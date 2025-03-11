4761fcda5102e357838c58093929c6bfe5a4d137
Mury05 <murielleodilone@gmail.com>
2025-03-11 15:04:01 +0100

    Ajout de la politique d'accès pour les articles, permettant de gérer les autorisations de création, mise à jour et suppression par les utilisateurs administrateurs.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Policies\ArticlePolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define("see-admin-menu", function($user){
+            return $user->admin;
+        });
+        Gate::define("create", [ArticlePolicy::class, 'create']);
+        Gate::define("update", [ArticlePolicy::class, 'update']);
+        Gate::define("delete", [ArticlePolicy::class, 'delete']);
     }
 }
